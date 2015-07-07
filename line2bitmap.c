e246c923cf658787c3a16b3287fe944cfeb804c1
cbdev <cb@cbcdn.com>
2015-07-07 02:28:08 +0200

    Create line2bitmap tool, fixes #5

---
+++ line2bitmap.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+	unsigned bar_width = 1, image_height = 64, i;
+	FILE* source = stdin;
+	unsigned x, y;
+
+	for(i=1; i<argc; i++){
+		if(!strcmp(argv[i], "--width")){
+			if(i + 1 < argc){
+				bar_width = strtoul(argv[i + 1], NULL, 10);
+				i++;
+			}
+			else{
+				fprintf(stderr, "Missing argument for --width\n");
+				return 1;
+			}
+		}
+		if(!strcmp(argv[i], "--height")){
+			if(i + 1 < argc){
+				image_height = strtoul(argv[i + 1], NULL, 10);
+				i++;
+			}
+			else{
+				fprintf(stderr, "Missing argument for --height\n");
+				return 1;
+			}
+		}
+		else{
+			if(source && source != stdin){
+				fclose(source);
+			}
+
+			source = fopen(argv[i], "r");
+		}
+	}
+
+	if(!source){
+		fprintf(stderr, "Failed to open input file\n");
+		return 1;
+	}
+
+	for(i = fgetc(source); i && i != EOF && i != '\n'; i = fgetc(source)){
+		for(x = 0; x < bar_width; x++){
+			for(y = 0; y < image_height; y++){
+				fputc(i, stdout);
+			}
+			fputc('\n', stdout);
+		}
+	}
+
+	fclose(source);
+	return 0;
+}
--- makefile
+++ makefile
@@ -11,7 +11,10 @@ pt1230: pt1230.c
 textlabel: textlabel.c
 	$(CC) $(CFLAGS) -I/usr/include/freetype2 -lfreetype -lfontconfig -o $@ $<
 
+line2bitmap: line2bitmap.c
+
 clean:
 	-rm interactive
 	-rm pt1230
 	-rm textlabel
+	-rm line2bitmap
